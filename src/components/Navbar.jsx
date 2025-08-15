59523666bd4cef9ccf08e14b9c4e3f3e119cfa01
PC 100 <b.oliinyk@gmail.com>
2025-08-15 23:59:46 +0300

    Commit Navbar menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,9 @@ const Navbar =() => {
       <nav className='nav'>
         <div>Profile</div>
         <div>Message</div>
+        <div>News</div>
+        <div>Music</div>
+        <div>Settings</div>
       </nav>
     )
 }
